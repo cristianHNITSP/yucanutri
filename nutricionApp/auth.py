b5a1a3cd87a40df65856cafb68f86b1965159ae6
Fractrhythm <cristian.hernandez.240602@gmail.com>
2024-10-23 11:59:34 -0600

    Actualización más reciente

--- nutricionApp/auth.py
+++ nutricionApp/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, session, flash, redirect, url_for, render_template
 from werkzeug.security import check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import Config  # Asegúrate de que esta importación sea correcta
 
 bp = Blueprint('auth', __name__)
@@ -103,7 +104,6 @@ def inicio_sesion():
                             flash('Correo o contraseña incorrectos', 'error')
                 
                 # Si no se ha encontrado un match en los roles o contraseñas
-                flash('Correo o contraseña incorrectos', 'error')
                 print("Credenciales incorrectas")
             else:
                 flash('Usuario no encontrado', 'error')
--- nutricionApp/nutriologo_paciente.py
+++ nutricionApp/nutriologo_paciente.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+import Config as Config
+from werkzeug.security import generate_password_hash
+
 
 bp = Blueprint('nutriologo_paciente', __name__, url_prefix='/nutriologo_paciente')
 
 
 @bp.route('/index_informacion')
 def index_informacion():
-    return render_template("index_informacion.html")
+    paciente_info = session.get('paciente_info')
+    print(f"Datos del paciente actual: {paciente_info}")
+    return render_template("index_informacion.html",)
