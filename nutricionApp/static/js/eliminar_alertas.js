dd380df04c5def4bbb93ffe9510b5e0280f854d1
Carlos E Valencia <choterifa@gmail.com>
2024-10-29 15:00:27 -0600

    cabmiso deiseño en tablas

--- nutricionApp/static/js/eliminar_alertas.js
+++ nutricionApp/static/js/eliminar_alertas.js
@@ -1,13 +1,18 @@
-// Esperar 3 segundos antes de cerrar automáticamente la alerta de actualización
 function cerrarAlertas() {
     setTimeout(function () {
         var alertas = document.querySelectorAll('.alert-dismissible'); // Seleccionar todas las alertas con botón de cierre
+        var contenedor = document.querySelector('.mensajes'); // Seleccionar el contenedor de las alertas
+
         alertas.forEach(function (alerta) {
             alerta.classList.remove('show'); // Ocultar la alerta
             setTimeout(function () {
                 alerta.remove(); // Eliminar la alerta del DOM después de la animación
-            }, 240); // Esperar 1 segundo después de la animación de desvanecimiento antes de eliminar el elemento
+                if (contenedor && contenedor.querySelectorAll('.alert-dismissible').length === 0) {
+                    contenedor.remove(); // Eliminar el contenedor si no quedan alertas
+                }
+            }, 240); // Esperar 240 ms después de la animación de desvanecimiento antes de eliminar el elemento
         });
-    }, 5500); // Esperar 3 segundos antes de ejecutar la función
+    }, 4500); // Esperar 5.5 segundos antes de ejecutar la función
 }
+
 cerrarAlertas();
